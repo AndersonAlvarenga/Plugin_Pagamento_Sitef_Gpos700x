0565b9cc2c7a7b4acd67f238c082899d5e845ef8
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-10-05 10:22:01 -0300

    Voltando para tela de pagamento Separada

--- www/main.js
+++ www/main.js
@@ -3,9 +3,6 @@ var exec = require('cordova/exec');
 exports.checarPagamento = function (success, error) {
     exec(success, error, 'MainActivity', 'checarPagamento');
 };
-exports.testePagamento = function (success, error) {
-    exec(success, error, 'MainActivity', 'testePagamento');
-};
 
 exports.imprimir = function (params, success, error) {
     exec(success, error, 'MainActivity', 'imprimir', [params]);
