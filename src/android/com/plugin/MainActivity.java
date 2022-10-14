a90bed4cdffd1ab424d4cc831d8ec8b5766f6740
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-10-14 15:46:07 -0300

    SETADO VARIAVEIS DE RETORNO COM VAZIO E VARIAVEL TITULO COMO TRANSACAO CANCELADA

--- src/android/com/plugin/MainActivity.java
+++ src/android/com/plugin/MainActivity.java
@@ -215,9 +215,9 @@ public class MainActivity extends CordovaPlugin implements ICliSiTefListener{
             try{
                 int i = this.cliSiTef.abortTransaction(-1);
                 try{
-                    this.cliSiTef.finishTransaction(-1);
+                    this.cliSiTef.finishTransaction(1);
                 }catch (Exception e){
-                    
+                    Log.e("ErroLog",e.getMessage());
                 }
 
                 this.titulo="";
